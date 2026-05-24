d9488783fef303fd2b3cab7c90ef5005daa07aa4
sunyuyang19910808 <sunyuyang_1991@126.com>
2026-05-24 16:16:23 +0800

    添加 原始DeepCAD训练的loss记录

--- train.py
+++ train.py
@@ -1,48 +1,67 @@
+import csv
+import os
 from collections import OrderedDict
+
 from tqdm import tqdm
-import argparse
+
 from dataset.cad_dataset import get_dataloader
 from config import ConfigAE
-from utils import cycle
+from utils import cycle, ensure_dir
 from trainer import TrainerAE
 
 
+def append_csv_row(csv_path, row):
+    ensure_dir(os.path.dirname(csv_path))
+    write_header = not os.path.exists(csv_path)
+    with open(csv_path, "a", newline="", encoding="utf-8") as file_obj:
+        writer = csv.DictWriter(file_obj, fieldnames=list(row.keys()))
+        if write_header:
+            writer.writeheader()
+        writer.writerow(row)
+
+
 def main():
-    # create experiment cfg containing all hyperparameters
     cfg = ConfigAE('train')
 
-    # create network and training agent
     tr_agent = TrainerAE(cfg)
 
-    # load from checkpoint if provided
     if cfg.cont:
         tr_agent.load_ckpt(cfg.ckpt)
 
-    # create dataloader
     train_loader = get_dataloader('train', cfg)
     val_loader = get_dataloader('validation', cfg)
     val_loader_all = get_dataloader('validation', cfg)
     val_loader = cycle(val_loader)
 
-    # start training
     clock = tr_agent.clock
+    metrics_csv = os.path.join(cfg.exp_dir, 'artifacts', 'train_metrics.csv')
 
-    for e in range(clock.epoch, cfg.nr_epochs):
-        # begin iteration
+    for e in range(clock.epoch, cfg.nr_epochs + 1):
         pbar = tqdm(train_loader)
         for b, data in enumerate(pbar):
-            # train step
             outputs, losses = tr_agent.train_func(data)
+            train_loss_cmd = losses['loss_cmd'].item()
+            train_loss_args = losses['loss_args'].item()
 
             pbar.set_description("EPOCH[{}][{}]".format(e, b))
             pbar.set_postfix(OrderedDict({k: v.item() for k, v in losses.items()}))
 
-            # validation step
             if clock.step % cfg.val_frequency == 0:
                 data = next(val_loader)
-                outputs, losses = tr_agent.val_func(data)
+                tr_agent.val_func(data)
 
             clock.tick()
+
+            append_csv_row(
+                metrics_csv,
+                {
+                    'epoch': e,
+                    'step': clock.step,
+                    'loss': train_loss_cmd + train_loss_args,
+                    'loss_cmd': train_loss_cmd,
+                    'loss_args': train_loss_args,
+                },
+            )
 
             tr_agent.update_learning_rate()
 
@@ -54,7 +73,6 @@ def main():
         if clock.epoch % cfg.save_frequency == 0:
             tr_agent.save_ckpt()
 
-        # if clock.epoch % 10 == 0:
         tr_agent.save_ckpt('latest')
 
 
